586322adf6f186aa562325eac3cbd4f160765b72
Cocomilk2908 <natasyaSyarafi@gmail.com>
2024-11-13 20:46:58 +0700

    UTS Natasya Syarafi

---
+++ app/src/main/java/com/example/quizormmahasiswa/APPDatabase.java
@@ -0,0 +1,22 @@
+package com.example.quizormmahasiswa;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {Mahasiswa.class}, version = 1)
+public abstract class APPDatabase extends RoomDatabase {
+    private static APPDatabase instance;
+
+    public abstract MahasiswaDAO mahasiswaDao();
+
+    public static synchronized APPDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                            APPDatabase.class, "mahasiswa_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
+}
---
+++ app/src/main/java/com/example/quizormmahasiswa/MahasiswaDAO.java
@@ -0,0 +1,24 @@
+package com.example.quizormmahasiswa;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface MahasiswaDAO {
+    @Insert
+    void insert(Mahasiswa Mahasiswa);
+
+    @Update
+    void update(Mahasiswa Mahasiswa);
+
+    @Delete
+    void delete(Mahasiswa Mahasiswa);
+
+    @Query("SELECT * FROM Mahasiswa")
+    List<Mahasiswa> getAllMahasiswa();
+
+}
